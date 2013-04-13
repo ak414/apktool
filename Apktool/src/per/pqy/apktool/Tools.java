0922fba3903c626d2981a0e9b04411c4836b82a8
dongfangxunlei <dongfangxunlei@gmail.com>
2013-04-13 21:31:32 +0800

    Initial Branch

---
+++ Apktool/src/per/pqy/apktool/Tools.java
@@ -0,0 +1,26 @@
+package per.pqy.apktool;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+
+public class Tools {
+	public static Drawable getApkIcon(Context context, String apkPath) {
+        PackageManager pm = context.getPackageManager();
+        PackageInfo info = pm.getPackageArchiveInfo(apkPath,
+                PackageManager.GET_ACTIVITIES);
+        if (info != null) {
+            ApplicationInfo appInfo = info.applicationInfo;
+            appInfo.sourceDir = apkPath;
+            appInfo.publicSourceDir = apkPath;
+            try {
+                return appInfo.loadIcon(pm);
+            } catch (OutOfMemoryError e) {
+             //   Log.e("ApkIconLoader", e.toString());
+            }
+        }
+        return null;
+    }
+}
